53e30fd4d0e88a861d0f67c2de76f7c9f614dfd2
Jo Verwohlt <57894802+joverwohlt@users.noreply.github.com>
2023-08-03 13:33:41 +0200

    Added ETHOS flag, ETHOS parameters in Cosmo_Params

--- zeus21/inputs.py
+++ zeus21/inputs.py
@@ -12,12 +12,13 @@ from . import constants
 import numpy as np
 from classy import Class
 from scipy.interpolate import interp1d
-
+from astropy.cosmology import FlatLambdaCDM
+from astropy.cosmology import Planck18, z_at_value
 
 class Cosmo_Parameters_Input:
     "Class to pass the 6 LCDM parameters as input"
 
-    def __init__(self, omegab= 0.0223828, omegac = 0.1201075, h_fid = 0.67810, As = 2.100549e-09, ns = 0.9660499, tau_fid = 0.05430842, kmax_CLASS = 500., zmax_CLASS = 50., Flag_emulate_21cmfast = False):
+    def __init__(self, omegab= 0.0223828, omegac = 0.1201075, h_fid = 0.67810, As = 2.100549e-09, ns = 0.9660499, tau_fid = 0.05430842, kmax_CLASS = 800., zmax_CLASS = 50.,zmin_CLASS = 5., Flag_emulate_21cmfast = False,):
 
         self.omegab = omegab
         self.omegac = omegac
@@ -29,6 +30,7 @@ class Cosmo_Parameters_Input:
         #other params for CLASS
         self.kmax_CLASS = kmax_CLASS
         self.zmax_CLASS = zmax_CLASS
+        self.zmin_CLASS = zmin_CLASS
         #and whether to emulate 21cmFAST
         self.Flag_emulate_21cmfast = Flag_emulate_21cmfast #whether to emulate 21cmFAST in HMF, LyA, and X-ray opacity calculations
 
@@ -37,7 +39,9 @@ class Cosmo_Parameters_Input:
 class Cosmo_Parameters:
     "Class that will keep the cosmo parameters throughout"
 
-    def __init__(self, CosmoParams_input, ClassCosmo):
+    def __init__(self, CosmoParams_input, ClassCosmo, cosmo=None, h_peak=None, k_peak=None, window_function='Smooth', logk=False, N_k=10000, Flag_ETHOS=False):
+
+
 
         self.omegab = CosmoParams_input.omegab
         self.omegac = CosmoParams_input.omegac
@@ -49,8 +53,26 @@ class Cosmo_Parameters:
         #other params in the input
         self.kmax_CLASS = CosmoParams_input.kmax_CLASS
         self.zmax_CLASS = CosmoParams_input.zmax_CLASS
-        self.zmin_CLASS = 4.0 #when to start the HMF calcs., not an input strictly
+        self.zmin_CLASS = CosmoParams_input.zmin_CLASS #when to start the HMF calcs., not an input strictly
         self.Flag_emulate_21cmfast = CosmoParams_input.Flag_emulate_21cmfast #whether to emulate 21cmFAST in HMF, LyA, and X-ray opacity calculations
+
+
+        #ETHOS param
+
+        self.Flag_ETHOS = Flag_ETHOS
+        self.cosmo = cosmo
+        if self.cosmo is None:
+            self.cosmo = Planck18
+
+        self.window_function = window_function
+        self.k_peak = k_peak
+        self.h_peak = h_peak
+        self.logk = logk
+        self.N_k = N_k
+
+        if self.Flag_ETHOS == True:
+            if k_peak is None or h_peak is None:
+                print('ERROR, need to pick both h_peak and k_peak if Flag_ETHOS=True')
 
         #derived params
         self.omegam = self.omegab + self.omegac
@@ -76,8 +98,12 @@ class Cosmo_Parameters:
 
 
         self._ztabinchi = np.linspace(0.0, 100. , 10000) #cheap so do a lot
-        self._chitab = ClassCosmo.z_of_r(self._ztabinchi)[0]
+        # self._chitab = ClassCosmo.z_of_r(self._ztabinchi)[0]
+        # self.zfofRint = interp1d(self._chitab, self._ztabinchi)
+        self._chitab, self._Hztab = ClassCosmo.z_of_r(self._ztabinchi) #chi and dchi/dz
         self.zfofRint = interp1d(self._chitab, self._ztabinchi)
+        self.chiofzint = interp1d(self._ztabinchi,self._chitab)
+        self.Hofzint = interp1d(self._ztabinchi,self._Hztab)
 
         _thermo = ClassCosmo.get_thermodynamics()
         self.Tadiabaticint = interp1d(_thermo['z'], _thermo['Tb [K]'])
@@ -107,7 +133,7 @@ class Cosmo_Parameters:
 
         #HMF-related constants
         if(self.Flag_emulate_21cmfast == False): #standard, best fit ST from Schneider+
-            self.a_ST = 0.707 #0.85 to fit 1805.00021, 0.707 for original ST
+            self.a_ST = 0.707 #OG ST fit, or 0.85 to fit 1805.00021
             self.p_ST = 0.3
             self.Amp_ST = 0.3222
             self.delta_crit_ST = 1.686
@@ -126,7 +152,9 @@ class Cosmo_Parameters:
 class Astro_Parameters:
     "Class to pass the astro parameters as input"
 
-    def __init__(self, Cosmo_Parameters, astromodel = 0, epsstar = 0.1, alphastar = 0.5, betastar = -0.5, Mc = 3e11, fesc10 = 0.1, alphaesc = 0.0, L40_xray = 3.0, E0_xray = 500., alpha_xray = -1.0, Emax_xray_norm=2000, Nalpha_lyA = 9690, Mturn_fixed = None, FLAG_MTURN_SHARP= False, accretion_model = 1):
+    def __init__(self, Cosmo_Parameters, astromodel = 0, epsstar = 0.1, alphastar = 0.5, betastar = -0.5, Mc = 3e11, fesc10 = 0.1, alphaesc = 0.0, \
+                 L40_xray = 3.0, E0_xray = 500., alpha_xray = -1.0, Emax_xray_norm=2000, Nalpha_lyA = 9690, Mturn_fixed = None, FLAG_MTURN_SHARP= False, \
+                    accretion_model = 0, sigmaUV=0.5, C0dust = 4.43, C1dust = 1.99):
 
 
         if(Cosmo_Parameters.Flag_emulate_21cmfast==True and astromodel == 0):
@@ -141,6 +169,7 @@ class Astro_Parameters:
         self.alphastar = alphastar #powerlaw index for lower masses
         self.betastar = betastar #powerlaw index for higher masses, only for model 0
         self.Mc = Mc # mass at which the power law cuts, only for model 0
+        self.sigmaUV = sigmaUV #stochasticity (gaussian rms) in the halo-galaxy connection P(MUV | Mh) - TODO: only used in UVLF not sfrd
 
         if self.astromodel == 0: #GALUMI-like
             self.accretion_model = accretion_model #0 = exponential, 1= EPS #choose the accretion model. Default = EPS
@@ -190,6 +219,10 @@ class Astro_Parameters:
             self.FLAG_MTURN_FIXED = True #whether to fix Mturn or use Matom(z) at each z
             self.Mturn_fixed = Mturn_fixed
             self.FLAG_MTURN_SHARP = FLAG_MTURN_SHARP #whether to do sharp cut at Mturn_fixed or regular exponential cutoff. Only active if FLAG_MTURN_FIXED and turned on by hand.
+
+        #dust parameters for UVLFs:
+        self.C0dust, self.C1dust = C0dust, C1dust #4.43, 1.99 is Meurer99; 4.54, 2.07 is Overzier01
+        self._kappaUV = 1.15e-28 #SFR/LUV, value from Madau+Dickinson14, fully degenerate with epsilon
 
 
 
